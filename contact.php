8bb3745af866efac09752358304db598c52c25ad
CorentinPolard <corentinpolard@gmail.com>
2025-01-23 15:47:31 +0100

    Construction de fichiers PHP contenant la structure initiale d'une page web

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php 
+$titre = "Contacts";
+include_once "header.php";
+?>
+</header>
+
+<a href="./">Retourner sur la page d'accueil</a>
+
+<?php include_once "footer.php" ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+$titre = "Accueil";
+include_once "header.php";
+?>
+
+<button class="btn btn-primary">test</button>
+<a href="./contact.php">Contacts</a>
+
+<?php include_once "footer.php" ?>
